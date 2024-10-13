bb326a45048058e521271c8f8562ad7be3c0a05b
Adrian Kuta <adrian.kuta93@gmail.com>
2024-10-13 18:13:13 +0200

    Add splash screen

--- app/src/main/kotlin/dev/adriankuta/partymania/MainActivity.kt
+++ app/src/main/kotlin/dev/adriankuta/partymania/MainActivity.kt
@@ -3,15 +3,33 @@ package dev.adriankuta.partymania
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import dev.adriankuta.partymania.core.ui.theme.PartyManiaTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        var isLoading: Boolean by mutableStateOf(true)
+
+        lifecycleScope.launch {
+            delay(500)
+            isLoading = false
+        }
+
+        splashScreen.setKeepOnScreenCondition {
+            isLoading
+        }
+
         setContent {
             PartyManiaTheme {
                 PartyManiaNavGraph()
